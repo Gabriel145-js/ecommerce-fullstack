91acbecee5d4b970052fc18b567b10a9c404531b
Gabriel145-js <gabrielzapa456@gmail.com>
2025-08-07 19:48:05 -0300

    fix: correção nomenclatura arquivo navbar.scss

--- frontend/src/components/NavBar/Navbar.jsx
+++ frontend/src/components/NavBar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {NavLink} from 'react-router-dom'
-import styles from './NavBar.module.scss'
+import styles from './Navbar.module.scss'
 import lupaIcon from '../../assets/icons/lupaIcon.svg'
 
 const Navbar = () => {
