eb20410d0f9e54ab017e1c861203d8b538e23c90
Gabriel145-js <gabrielzapa456@gmail.com>
2025-08-07 17:21:55 -0300

    feat: navbar com links, logo e icones

---
+++ frontend/src/components/NavBar/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import {NavLink} from 'react-router-dom'
+import styles from './NavBar.module.scss'
+import lupaIcon from '../../assets/icons/lupaIcon.svg'
+
+const Navbar = () => {
+  return (
+<nav className={styles.navbarContainer}>
+  <div className={styles.navbarContent}>
+    <h1 className={styles.logo}>Elegante</h1>
+
+    <div className={styles.navbarLinks}>
+      <NavLink>Novidades</NavLink>
+      <NavLink>Blusas</NavLink>
+      <NavLink>Calças</NavLink>
+      <NavLink>Todos os produtos</NavLink>
+    </div>
+
+    <div className={styles.navbarIcons}>
+
+      <img src={lupaIcon} alt="Search"  />
+    </div>
+  </div>
+</nav>
+  )
+}
+
+export default Navbar
