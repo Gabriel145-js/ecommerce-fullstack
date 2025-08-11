6decdeb0f793652fac11346adb645209356b9c75
Gabriel145-js <gabrielzapa456@gmail.com>
2025-08-11 10:19:32 -0300

    feat: adicionado a rota para adicionar produto

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,9 @@
 import { useState } from 'react'
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import IndexPage from './pages/index/IndexPage'
-import Navbar from './components/NavBar/Navbar'
+import DashboardAdmin from './pages/Admin/DashboardAdmin'
+import AddProduto from './components/Admin/AddProduto/AddProduto'
+import NavBar from './components/NavBar/NavBar' 
 
 
 
@@ -10,9 +12,13 @@ function App() {
   return (
     <>
     <BrowserRouter>
-    <Navbar/>
+      <NavBar/>
       <Routes>
         <Route path = "/" element={<IndexPage/>}/>
+
+        <Route path='/DashboardAdmin' element={<DashboardAdmin/>}>
+            <Route path='AddProduto' element={<AddProduto/>}/>
+        </Route>
       </Routes>
     </BrowserRouter>
       
