44ee0c1c30fdfc4447bc29a3028f148bc1a1b3db
Gabriel145-js <gabrielzapa456@gmail.com>
2025-08-11 22:05:03 -0300

    feat: implementação do POST para criar produtos pelo <form>

--- frontend/src/components/Admin/AddProduto/AddProduto.jsx
+++ frontend/src/components/Admin/AddProduto/AddProduto.jsx
@@ -4,10 +4,42 @@ import SelecionaCategoria from './Categorias/SelecionaCategoria'
 
 const AddProduto = () => {
 
-const handleSubmit = (e) => {
-  e.preventDefault()
-}
-  
+  const [nomeProduto, setNomeProduto] = useState('')
+  const [estoque, setEstoque] = useState('')
+  const [preco, setPreco] = useState('')
+  const [descricao, setDescricao] = useState('')
+
+  const urlProdutos = 'http://localhost:5000/api/produtos'
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    const criarProduto = async () => {
+      try {
+        const res = await fetch(urlProdutos, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ nome: nomeProduto, descricao: descricao, preco: preco, estoque: estoque })
+        })
+
+        const data = await res.json()
+        console.log('criado', data)
+        setNomeProduto('')
+        setDescricao('')
+        setPreco('')
+        setEstoque('')
+
+
+      } catch (error) {
+        console.error(error)
+
+      }
+    }
+    criarProduto()
+  }
+
   return (
     <div className={styles.containerAddProduto}>
       <header className={styles.titESubtitulo}>
@@ -16,46 +48,41 @@ const handleSubmit = (e) => {
       </header>
 
       <section className={styles.quadroAdicionarProduto}>
-
         <form className={styles.formProduto} onSubmit={handleSubmit}>
-
-
           <label className={styles.labelInput}>
             <span>Nome</span>
-            <input type="text" placeholder='Nome do produto' />
+            <input type="text" placeholder='Nome do produto' value={nomeProduto} onChange={e => setNomeProduto(e.target.value)} />
           </label>
 
           <label className={styles.labelInput}>
             <span>Descrição</span>
-            <input type="text" placeholder='Descrição' />
+            <input type="text" placeholder='Descrição' value={descricao} onChange={e => setDescricao(e.target.value)} />
           </label>
 
           <label className={styles.labelInput}>
             <span>Preço</span>
-            <input type="number" placeholder='Preço' />
-          </label>
-
-         <SelecionaCategoria/>
-
-          <label className={styles.labelInput}>
-            <span>Imagem pricipal</span>
-            <input type="file" id="foto" name="foto" accept="image/*" required />
-          </label>
-
-          <label className={styles.labelInput}>
-            <span>Imagens adicionais</span>
-            <input type="file" id="foto" name="foto" accept="image/*" required />
+            <input type="number" placeholder='Preço' value={preco} onChange={e => setPreco(e.target.value)} />
           </label>
 
           <label className={styles.labelInput}>
             <span>Estoque</span>
-            <input type="number" placeholder='Quantidade em estoque' />
+            <input type="number" placeholder='Quantidade em estoque' value={estoque} onChange={e => setEstoque(e.target.value)} />
           </label>
 
+          <label className={styles.labelInput}>
+            <span>Imagem principal</span>
+            <input type="file" accept="image/*" />
+          </label>
+
+          <label className={styles.labelInput}>
+            <span>Imagens adicionais</span>
+            <input type="file" accept="image/*" multiple />
+          </label>
+
+          <SelecionaCategoria />
+
           <button type="submit">Criar produto</button>
-
         </form>
-
       </section>
 
 
