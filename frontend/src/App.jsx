41cabe668746eefdece6946ea73b95969de9ac83
Gabriel145-js <gabrielzapa456@gmail.com>
2025-08-12 08:16:19 -0300

    fix: correção bug deploy Netlify(frontend)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import IndexPage from './pages/index/IndexPage'
 import DashboardAdmin from './pages/Admin/DashboardAdmin'
 import AddProduto from './components/Admin/AddProduto/AddProduto'
-import NavBar from './components/NavBar/NavBar' 
+import NavBar from './components/NavBar/NavBar.jsx' 
 
 
 
