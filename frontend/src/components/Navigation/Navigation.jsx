cc0a8fef041e64050ff07f55d87197f8b1518193
Gabriel145-js <gabrielzapa456@gmail.com>
2025-08-12 08:29:18 -0300

    fix: correção de eror 'sensitive case' da netlify

---
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import {Link, NavLink} from 'react-router-dom'
+import styles from './Navigation.module.scss'
+import lupaIcon from '../../assets/icons/lupaIcon.svg'
+import adminIcon from '../../assets/icons/adminIcon.svg'
+
+const Navigation = () => {
+  return (
+<nav className={styles.navbarContainer}>
+  <div className={styles.navbarContent}>
+    <h1 className={styles.logo}>Elegante</h1>
+
+    <div className={styles.navbarLinks}>
+      <NavLink>Novidades</NavLink>
+      <NavLink>Blusas</NavLink>
+      <NavLink>Calças</NavLink>
+      <NavLink>Todos os produtos</NavLink>
+    </div>
+
+    <div className={styles.navbarIcons}>
+
+      <NavLink >
+        <img src={lupaIcon} alt="Icone Busca"  />
+      </NavLink>
+
+      <NavLink to='/DashboardAdmin'>
+        <img  src={adminIcon} alt="Icone Admin" />
+      </NavLink>
+    </div>
+  </div>
+</nav>
+  )
+}
+
+export default Navigation
