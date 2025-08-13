c778ffc7a312ad094b2e02280b0a75660c878e4b
Gabriel145-js <gabrielzapa456@gmail.com>
2025-08-13 14:49:21 -0300

    feat: adicionado novas colunas no banco, ajustada a rota para POST das novas colunas

--- backend/routes/produtos.js
+++ backend/routes/produtos.js
@@ -1,19 +1,45 @@
-const db = require('../db/db')
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const db = require('../db/db');
+
+const router = express.Router();
 
 router.post('/', async (req, res) => {
-    const {nome, descricao, preco, estoque} = req.body
-    try {
-        const criarProduto = await db`INSERT INTO produtos (nome, descricao, preco, estoque) VALUES (${nome}, ${descricao}, ${preco}, ${estoque}) RETURNING *`
+  const {
+    nome,
+    descricao,
+    preco,
+    estoque,
+    imagem_principal,
+    imagens_adicionais,
+    tamanho_p,
+    tamanho_m,
+    tamanho_g,
+    categorias_id
+  } = req.body;
 
-        res.status(201).json(criarProduto)
+  try {
+    const imagensArray = `{${imagens_adicionais.join(',')}}`;
 
-    } catch (error) {
-        console.error(error)
-        res.status(500).json({error: 'Erro ao criar produto'})
+    const criarProduto = await db`
+    INSERT INTO produtos (
+    nome, descricao, preco, estoque,
+    imagem_principal, imagens_adicionais,
+    tamanho_p, tamanho_m, tamanho_g,
+    categorias_id
+  ) VALUES (
+    ${nome}, ${descricao}, ${preco}, ${estoque},
+    ${imagem_principal}, ${imagensArray},
+    ${tamanho_p}, ${tamanho_m}, ${tamanho_g},
+    ${categorias_id}
+  )
+  RETURNING *;
+`;
 
-    }
-})
+    res.status(201).json(criarProduto);
+  } catch (error) {
+    console.error('Erro ao criar produto:', error.message);
+    res.status(500).json({ error: error.message });
+  }
+});
 
-module.exports = router
+module.exports = router;
