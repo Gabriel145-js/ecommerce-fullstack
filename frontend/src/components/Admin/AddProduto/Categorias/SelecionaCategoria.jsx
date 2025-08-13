d66d063f14b989fc478c1f903e12306609ea740c
Gabriel145-js <gabrielzapa456@gmail.com>
2025-08-13 14:51:53 -0300

    refactor: refatorado os estados da seleção de categorias para usar como prop no AddProduto

--- frontend/src/components/Admin/AddProduto/Categorias/SelecionaCategoria.jsx
+++ frontend/src/components/Admin/AddProduto/Categorias/SelecionaCategoria.jsx
@@ -1,10 +1,9 @@
 import React, { useState, useEffect } from 'react'
 import styles from '../AddProduto.module.scss'
 
-const SelecionaCategoria = () => {
+const SelecionaCategoria = ({ categorias,setCategorias, selecionaCategoria, setSelecionaCategoria }) => {
     const [novaCategoria, setNovaCategoria] = useState('')
-    const [categorias, setCategorias] = useState([])
-    const [selecionaCategoria, setSelecionaCategoria] = useState('')
+  
     const [modalAberto, setModalAberto] = useState(false)
 
     const urlCategorias = 'http://localhost:5000/api/categorias'
@@ -97,7 +96,7 @@ const SelecionaCategoria = () => {
                     </select>
                 </label>
 
-             
+
             </div>
 
             {/* Modal para criar categoria */}
