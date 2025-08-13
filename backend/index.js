8fd2439de52df008205455893558d9abdb7feaac
Gabriel145-js <gabrielzapa456@gmail.com>
2025-08-13 14:37:59 -0300

    feat: rota para Cloudnary que está armazenando as fotos dos produtos

--- backend/index.js
+++ backend/index.js
@@ -1,27 +1,38 @@
 const express = require('express')
 const cors = require('cors')
 require('dotenv').config();
+const cloudinary = require('cloudinary').v2;
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+
+//Cloudnary configs
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
 
 // Importar rotas de categorias
 const categoriasRoutes = require('./routes/categorias')
 const produtosRoutes = require('./routes/produtos')
+const imagensProdutos = require('./routes/imagens')
 
 // Usar as rotas de categorias
 app.use('/api/categorias', categoriasRoutes)
 app.use('/api/produtos', produtosRoutes)
+app.use('/api/imagens', imagensProdutos)
 
-// Rota de teste
 
-app.listen(5000, () => {
-    console.log('Servidor rodando na porta 5000')
-    console.log('Acesse: http://localhost:5000')
-    console.log('Categorias: http://localhost:5000/api/categorias')
-    console.log('Categorias: http://localhost:5000/api/produtos')
+//PORT é defenido no Render
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
 
-    console.log('database', process.env.DATABASE_URL)
-})
+
+
