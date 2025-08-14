227f3db9e263ff28428cfe27f4c5b58cfc19ca42
Gabriel145-js <gabrielzapa456@gmail.com>
2025-08-14 17:18:32 -0300

    refactor: refatoração do codigo para para exibir o componente ProdutoList

--- frontend/src/pages/index/IndexPage.jsx
+++ frontend/src/pages/index/IndexPage.jsx
@@ -1,77 +1,15 @@
 import React, { useState, useEffect } from 'react'
 import styles from './IndexPage.module.scss'
+import ProdutosList from '../../components/Produtos/ProdutosList'
 
 const IndexPage = () => {
-  const [produtos, setProdutos] = useState([])
-  
-  const API_URL = import.meta.env.VITE_API_URL;
-  const urlProdutos = `${API_URL}/api/produtos`;
 
-  useEffect(() => {
-    const exibirProdutos = async () => {
-      try {
-        const res = await fetch(urlProdutos, {
-          method: 'GET',
-          headers: {
-            'Content-Type': 'application/json',
-          }
-
-        })
-
-        const data = await res.json()
-        setProdutos(data)
-
-      } catch (error) {
-        console.error('erro ao exibir produtos', error.message)
-
-
-      }
-    }
-    exibirProdutos()
-  }, [])
 
   return (
     <div>
 
+      <ProdutosList/>
 
-      <ul className={styles.cardProduto}>
-        {produtos.map(prod => (
-          <li key={prod.id} className={styles.produtoCard}>
-            <div className={styles.labels}>
-              {prod.novo && <span className={styles.labelNovo}>Novo</span>}
-              {prod.promocao && <span className={styles.labelPromocao}>Promoção</span>}
-            </div>
-
-            {prod.imagem_principal && (
-              <img src={prod.imagem_principal} alt={prod.nome} className={styles.produtoImagem} />
-            )}
-
-            <h2 className={styles.produtoNome}>{prod.nome}</h2>
-
-            {prod.avaliacao && (
-              <p className={styles.avaliacao}>
-                ⭐ {prod.avaliacao} ({prod.reviews} avaliações)
-              </p>
-            )}
-
-            <p className={styles.preco}>
-              {prod.preco_original && (
-                <span className={styles.precoOriginal}>R$ {prod.preco_original}</span>
-              )}
-              <span className={styles.precoPromocional}>R$ {prod.preco}</span>
-            </p>
-
-            <p>Estoque: {prod.estoque}</p>
-
-            <p>Tamanhos disponíveis:</p>
-            <ul className={styles.tamanhos}>
-              {prod.tamanho_p && <li>P</li>}
-              {prod.tamanho_m && <li>M</li>}
-              {prod.tamanho_g && <li>G</li>}
-            </ul>
-          </li>
-        ))}
-      </ul>
 
 
     </div>
