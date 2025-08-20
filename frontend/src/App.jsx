325a1a03c8ae75e6dda21519de7a906eabb6cb60
Gabriel145-js <gabrielzapa456@gmail.com>
2025-08-20 17:11:43 -0300

    feat: mudança nas rotas para que o footer não apareça na area de admin

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,38 @@
 import { useState } from 'react'
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom'
 import IndexPage from './pages/index/IndexPage'
 import DashboardAdmin from './pages/Admin/DashboardAdmin'
 import AddProduto from './components/Admin/AddProduto/AddProduto'
-import Navigation from './components/Navigation/Navigation' 
+import Navigation from './components/Navigation/Navigation'
 import GerenciarCategorias from './components/Admin/AddProduto/Categorias/GerenciarCategorias/GerenciarCategorias'
+import Footer from './components/Footer/Footer'
 
+function AppRoutes() {
+  const location = useLocation()
 
-
-function App() {
+  // Verifica se a rota atual começa com "/DashboardAdmin"
+  const isAdminRoute = location.pathname.startsWith('/DashboardAdmin')
 
   return (
     <>
-    <BrowserRouter>
-      <Navigation/>
+      <Navigation />
       <Routes>
-        <Route path = "/" element={<IndexPage/>}/>
-
-        <Route path='/DashboardAdmin' element={<DashboardAdmin/>}>
-            <Route path='AddProduto' element={<AddProduto/>}/>
-            <Route path='GerenciarCategorias' element={<GerenciarCategorias/>}/>
+        <Route path="/" element={<IndexPage />} />
+        <Route path="/DashboardAdmin" element={<DashboardAdmin />}>
+          <Route path="AddProduto" element={<AddProduto />} />
+          <Route path="GerenciarCategorias" element={<GerenciarCategorias />} />
         </Route>
       </Routes>
-    </BrowserRouter>
-      
-      
+      {!isAdminRoute && <Footer />}
     </>
+  )
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   )
 }
 
