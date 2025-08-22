a48041758b183b11340ffca564ced5124ba5eb3d
Gabriel145-js <gabrielzapa456@gmail.com>
2025-08-22 17:04:31 -0300

    feat: rota pro crud de path e delete adicionado

--- backend/routes/produtos.js
+++ backend/routes/produtos.js
@@ -54,4 +54,52 @@ router.post('/', async (req, res) => {
   }
 });
 
+router.patch('/:id', async (req, res) => {
+  const produtoId = parseInt(req.params.id)
+
+  const nomeFormatar = req.body.nome?.trim() || null
+  const precoFormatar = req.body.preco?.toString().trim() || null
+  const estoqueFormatar = req.body.estoque?.toString().trim() || null
+
+  try {
+    const atualizaProdutos = await db`
+      UPDATE produtos 
+      SET nome = ${nomeFormatar}, preco = ${precoFormatar}, estoque = ${estoqueFormatar}
+      WHERE id = ${produtoId}
+      RETURNING *`
+
+    console.log('Produto atualizado:', atualizaProdutos[0])
+
+    return res.status(200).json({
+      message: 'Produto atualizado com sucesso',
+      produto: atualizaProdutos[0]
+    })
+  } catch (error) {
+    console.error('Erro ao atualizar produto:', error.message)
+    return res.status(500).json({ error: 'Erro ao atualizar produto' })
+  }
+})
+
+router.delete('/:id', async (req, res) => {
+  const produtoId = parseInt(req.params.id)
+
+  try {
+    const deletaProduto = await db`DELETE FROM produtos WHERE id = ${produtoId}`
+
+    return res.json({
+      message: 'Produto deletado com sucesso',
+      id: produtoId,
+      details: deletaProduto
+
+    })
+
+
+  } catch (error) {
+    console.log('Erro interno no servidor', error.message)
+    res.status(500).json({ error: 'Erro interno no servidor' })
+
+  }
+
+})
+
 module.exports = router;
