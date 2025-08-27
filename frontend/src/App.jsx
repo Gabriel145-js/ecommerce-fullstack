c99726a568d303af7d80c64091200f836faa4bda
Gabriel145-js <gabrielzapa456@gmail.com>
2025-08-27 16:50:05 -0300

    feat: rota para exibir o Breadcrumb em todas as abas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ import GerenciarCategorias from './components/Admin/AddProduto/Categorias/Gerenc
 import Footer from './components/Footer/Footer'
 import TodosProdutos from './components/Admin/AddProduto/TodosProdutos/TodosProdutos'
 import DetalhesProdutos from './components/Produtos/DetalhesProdutos/DetalhesProdutos'
+import Breadcrumb from './components/Navigation/BreadCrumb/Breadcrumb'
 
 function AppRoutes() {
   const location = useLocation()
@@ -18,14 +19,15 @@ function AppRoutes() {
   return (
     <>
       <Navigation />
+      <Breadcrumb />
       <Routes>
         <Route path="/" element={<IndexPage />} />
         <Route path="/DashboardAdmin" element={<DashboardAdmin />}>
           <Route path="AddProduto" element={<AddProduto />} />
           <Route path="GerenciarCategorias" element={<GerenciarCategorias />} />
-          <Route path="TodosProdutos" element={<TodosProdutos/>} />
+          <Route path="TodosProdutos" element={<TodosProdutos />} />
         </Route>
-       <Route path="/Detalhes-Produto/:id/:nome" element={<DetalhesProdutos />} />
+        <Route path="/Detalhes-Produto/:id/:nome" element={<DetalhesProdutos />} />
       </Routes>
       {!isAdminRoute && <Footer />}
     </>
