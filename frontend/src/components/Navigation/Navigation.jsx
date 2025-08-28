7712f44b9a97348472078bcab39b33ea6c82d879
Gabriel145-js <gabrielzapa456@gmail.com>
2025-08-28 17:07:24 -0300

    refactor: refatoração da navbar feat: botão de carrinho de compras

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -1,34 +1,46 @@
-import React from 'react'
-import {Link, NavLink} from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link, NavLink } from 'react-router-dom'
 import styles from './Navigation.module.scss'
 import lupaIcon from '../../assets/icons/lupaIcon.svg'
 import adminIcon from '../../assets/icons/adminIcon.svg'
+import carCompras from '../../assets/icons/carCompras.svg'
+import CarrinhoCompras from '../CarrinhoCompras/CarrinhoCompras'
 
 const Navigation = () => {
+  const [carrinhoAberto, setCarrinhoAberto] = useState(false);
+
   return (
-<nav className={styles.navbarContainer}>
-  <div className={styles.navbarContent}>
-    <NavLink to='/' className={styles.logo}>Elegante</NavLink>
+    <>
+      <nav className={styles.containerNavbar}>
+        <div className={styles.conteudoNavbar}>
+          <NavLink to='/' className={styles.logo}>Elegante</NavLink>
 
-    <div className={styles.navbarLinks}>
-      <NavLink>Novidades</NavLink>
-      <NavLink>Blusas</NavLink>
-      <NavLink>Calças</NavLink>
-      <NavLink>Todos os produtos</NavLink>
-    </div>
+          <div className={styles.linksNavbar}>
+            <NavLink>Novidades</NavLink>
+            <NavLink>Blusas</NavLink>
+            <NavLink>Calças</NavLink>
+            <NavLink>Todos os produtos</NavLink>
+          </div>
 
-    <div className={styles.navbarIcons}>
+          <div className={styles.iconesNavbar}>
 
-      <NavLink >
-        <img src={lupaIcon} alt="Icone Busca"  />
-      </NavLink>
+            <button className={styles.botaoIcone}>
+              <img src={lupaIcon} alt="Icone Busca" />
+            </button>
 
-      <NavLink to='/DashboardAdmin'>
-        <img  src={adminIcon} alt="Icone Admin" />
-      </NavLink>
-    </div>
-  </div>
-</nav>
+            <button onClick={() => setCarrinhoAberto(true)} className={styles.botaoIcone}>
+              <img src={carCompras} alt="Icone Carrinho" />
+            </button>
+
+            <NavLink to='/DashboardAdmin' className={styles.botaoIcone}>
+              <img src={adminIcon} alt="Icone Admin" />
+            </NavLink>
+
+          </div>
+        </div>
+      </nav>
+      {carrinhoAberto && <CarrinhoCompras onClose={() => setCarrinhoAberto(false)} />}
+    </>
   )
 }
 
