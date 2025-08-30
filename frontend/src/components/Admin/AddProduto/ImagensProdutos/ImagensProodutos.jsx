7df337c8116876ea73af69ebfff92c3a16490ee0
Gabriel145-js <gabrielzapa456@gmail.com>
2025-08-30 17:21:57 -0300

    refactor: atualizado o layout para ficar um botao ao lado do outro de adição de fotos

--- frontend/src/components/Admin/AddProduto/ImagensProdutos/ImagensProodutos.jsx
+++ frontend/src/components/Admin/AddProduto/ImagensProdutos/ImagensProodutos.jsx
@@ -35,62 +35,64 @@ const ImagensProodutos = ({ imagemPrincipal, setImagemPrincipal, imagensAdiciona
 
     return (
         <>
-            <div className={styles.labelInput}>
-                <span>Imagem principal</span>
+            <section className={styles.sectionImagensProdutos}>
+                <div className={styles.labelInput} style={{ width: '100%' }}>
+                    <span>Imagem principal</span>
 
-                <button type="button" className={styles.customFileButton} onClick={triggerFileInputPrincipal}>
-                    <img className={styles.uploadIcon} src={uploadIcon} alt="" />
-                    {imagemPrincipal ? imagemPrincipal.name : 'Escolher imagem principal'}
-                </button>
-                <input
-                    type="file"
-                    accept="image/*"
-                    ref={fileInputPrincipalRef}
-                    onChange={handleImagemPrincipalChange}
-                    style={{ display: 'none' }}
-                />
-            </div>
+                    <button type="button" className={styles.customFileButton} onClick={triggerFileInputPrincipal}>
+                        <img className={styles.uploadIcon} src={uploadIcon} alt="" />
+                        {imagemPrincipal ? imagemPrincipal.name : 'Escolher imagem principal'}
+                    </button>
+                    <input
+                        type="file"
+                        accept="image/*"
+                        ref={fileInputPrincipalRef}
+                        onChange={handleImagemPrincipalChange}
+                        style={{ display: 'none' }}
+                    />
+                </div>
 
-            <div className={styles.labelInput}>
-                <span>Imagens adicionais </span>
-                <button type="button" className={styles.customFileButton} onClick={triggerFileInputAdicionais}>
-                    <img className={styles.uploadIcon} src={uploadIcon} alt="" />
-                    {imagensAdicionais.length > 0
-                        ? `${imagensAdicionais.length} imagem(ns) selecionada(s)`
-                        : 'Escolher imagens adicionais'
-                    }
-                </button>
-                <input
-                    type="file"
-                    accept="image/*"
-                    multiple
-                    ref={fileInputAdicionaisRef}
-                    onChange={handleImagensAdicionaisChange}
-                    style={{ display: 'none' }}
-                />
+                <div className={styles.labelInput} style={{ width: '100%' }}>
+                    <span>Imagens adicionais </span>
+                    <button type="button" className={styles.customFileButton} onClick={triggerFileInputAdicionais}>
+                        <img className={styles.uploadIcon} src={uploadIcon} alt="" />
+                        {imagensAdicionais.length > 0
+                            ? `${imagensAdicionais.length} imagem(ns) selecionada(s)`
+                            : 'Escolher imagens adicionais'
+                        }
+                    </button>
+                    <input
+                        type="file"
+                        accept="image/*"
+                        multiple
+                        ref={fileInputAdicionaisRef}
+                        onChange={handleImagensAdicionaisChange}
+                        style={{ display: 'none' }}
+                    />
 
 
-                {imagensAdicionais.length > 0 && (
-                    <div className={styles.selectedFiles}>
-                     
-                        {imagensAdicionais.map((file, index) => (
-                            <div key={index} className={styles.fileTag}>
-                                <span>{file.name}</span>
-                                <button
-                                    type="button"
-                                    className={styles.removeFileTagButton}
-                                    onClick={() => removeImagemAdicional(index)}
-                                >
-                                    ×
-                                </button>
-                            </div>
-                        ))}
-                    </div>
-                )}
-            </div>
+                    {imagensAdicionais.length > 0 && (
+                        <div className={styles.selectedFiles}>
 
-        </>
-    )
+                            {imagensAdicionais.map((file, index) => (
+                                <div key={index} className={styles.fileTag}>
+                                    <span>{file.name}</span>
+                                    <button
+                                        type="button"
+                                        className={styles.removeFileTagButton}
+                                        onClick={() => removeImagemAdicional(index)}
+                                    >
+                                        ×
+                                    </button>
+                                </div>
+                            ))}
+                        </div>
+                    )}
+                </div>
+                </section>
+
+                </>
+                )
 }
 
-export default ImagensProodutos
+                export default ImagensProodutos
