a4b7f99a13ed00cf3f436da16182b55c29ba4a86
Gabriel145-js <gabrielzapa456@gmail.com>
2025-09-05 17:16:39 -0300

    feat: componente reutilzavel para formulario de pedido

---
+++ frontend/src/components/OrdemConclusaoPedido/DefaultFormPedido.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import styles from './DefaultFormPedido.module.scss'
+
+const DefaultFormPedido = ({ titulo, icone, campos }) => {
+    return (
+
+        <section className={styles.containerSecao}>
+            <div className={styles.cardSecao}>
+                <header className={styles.tituloSecao}>
+                    <h1>
+                        {icone && <img src={icone} alt=''/>}
+                        {titulo}
+                    </h1>
+                </header>
+
+                <div className={styles.inputSecao}>
+                    {campos.map((campo, index) => (
+                        <label key={index} className={styles.labelCampo}>
+                            {campo.label}
+                            <input
+                                type={campo.tipo || 'text'}
+                                placeholder={campo.placeholder}
+                                value={campo.value}
+                                onChange={e => campo.onChange(e.target.value)}
+                            />
+                        </label>
+                    ))}
+                </div>
+            </div>
+        </section>
+
+    )
+}
+
+export default DefaultFormPedido
