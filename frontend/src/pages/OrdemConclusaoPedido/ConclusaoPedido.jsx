fe4d6e09b96a3d293066b1f99275d44c8bf79bae
Gabriel145-js <gabrielzapa456@gmail.com>
2025-09-09 17:19:52 -0300

    feat: adicionado loading, e botoes de proseguir e voltar dentro dos cards

--- frontend/src/pages/OrdemConclusaoPedido/ConclusaoPedido.jsx
+++ frontend/src/pages/OrdemConclusaoPedido/ConclusaoPedido.jsx
@@ -1,19 +1,24 @@
 import React, { useState, useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { CartContext } from '../../context/CartContext';
 import styles from './ConclusaoPedido.module.scss';
 import InfosPessoais from '../../components/OrdemConclusaoPedido/InfosPessoais/InfosPessoais';
 import EndereçoPedido from '../../components/OrdemConclusaoPedido/EndereçoPedido/EndereçoPedido';
 import ConcluirPedido from '../../components/OrdemConclusaoPedido/ConcluirPedido/ConcluirPedido';
+import Stepper from '../../components/Stepper/Stepper';
+import { Player } from '@lottiefiles/react-lottie-player';
 
-const nomePassos = ['Informações Pessoais', 'Endereço de Entrega', 'Revisão e Pagamento'];
+const nomePassos = ['Informações Pessoais', 'Endereço de Entrega', 'Pagamento'];
 
 const ConclusaoPedido = () => {
   const { carrinhoItens, totalPedido, limparCarrinho } = useContext(CartContext);
+  const navigate = useNavigate()
 
   const API_URL = import.meta.env.VITE_API_URL;
   const urlPedido = `${API_URL}/api/pedido`;
 
   const [passoAtual, setPassoAtual] = useState(0);
+  const [loading, setLoading] = useState(false)
 
   // Estados dos dados do cliente
   const [nomeCliente, setNomeCliente] = useState('');
@@ -62,7 +67,7 @@ const ConclusaoPedido = () => {
         cep: cep,
         estado: estado,
         cidade: cidade,
-        bairro: bairro ,
+        bairro: bairro,
         rua: rua,
         numero: numResidencia,
         complemento: complemento
@@ -71,134 +76,157 @@ const ConclusaoPedido = () => {
       valor_total: totalPedido
     };
 
+    //Faz o POST para o db
     const enviarPedido = async () => {
       try {
+        setLoading(true)
         const response = await fetch(urlPedido, {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json'
           },
           body: JSON.stringify(dadosPedido)
-        });
+        })
 
-        if (response.ok) {
-          const data = await response.json();
-          console.log('Pedido enviado:', data);
-          alert('Pedido enviado com sucesso!');
-          limparCarrinho();
-        } else {
-          const erro = await response.json();
-          console.error('Erro do servidor:', erro);
-          throw new Error('Falha ao enviar pedido');
-        }
+        const data = response.json()
+        limparCarrinho()
+
+        setTimeout(() => setLoading(false), 2000); // espera acabar os 2s para o loading ficar falso, ent ele redireciona para '/'
 
       } catch (error) {
         console.error('Erro ao enviar pedido:', error);
-        alert('Erro ao enviar pedido. Verifique o console para mais detalhes.');
+
       }
-    };
+    }
+
     enviarPedido();
   };
 
   return (
     <section className={styles.containerCncluirPedido}>
+
+      {loading && (
+        <div className={styles.animacaoSucessoOverlay}>
+          <Player
+            src="https://lottie.host/e28b09ea-ce40-47b9-8b7c-afa4ebcfe1e5/W0npmgm2Ad.json"
+            className={styles.animacaoSucesso}
+            autoplay
+            onEvent={(event) => {
+              if (event === 'complete') {
+                setTimeout(() => {
+                  setLoading(false);
+                  navigate('/');
+                }, 0);
+              }
+            }}
+          />
+        </div>
+      )}
       <header className={styles.tituloSubtitulo}>
-        <h1>Finalizar Pedido</h1>
+        <h1>Finalizar  Pedido</h1>
         <p>Complete seus dados para concluir a compra</p>
       </header>
 
-      <div className={styles.linhaDoTempo}>
-        {nomePassos.map((nome, index) => (
-          <div
-            key={nome}
-            className={`${styles.passo} ${index === passoAtual ? styles.passoAtivo : ''} ${index < passoAtual ? styles.passoCompleto : ''}`}
-          >
-            <div className={styles.numeroPasso}>{index + 1}</div>
-            <div className={styles.nomePasso}>{nome}</div>
-          </div>
-        ))}
-      </div>
-      
-      {/* Adicionado o resumo do pedido no passo de revisão */}
-      {passoAtual === 2 && (
-         <div className={styles.resumoCarrinho}>
-            <h2>Revisão do Pedido</h2>
-            {carrinhoItens.length > 0 ? (
-              <ul>
-                {carrinhoItens.map(item => (
-                  <li key={item.id}>
-                    <span>{item.nome} (x{item.quantidade})</span>
-                    <span>R$ {(item.preco * item.quantidade).toFixed(2)}</span>
-                  </li>
-                ))}
-              </ul>
-            ) : (
-              <p>Seu carrinho está vazio.</p>
-            )}
-            <h3>Total: R$ {totalPedido.toFixed(2)}</h3>
-          </div>
-      )}
+      <Stepper currentStep={passoAtual} />
+
 
       <div className={styles.conteudoPasso}>
         {passoAtual === 0 && (
-          <InfosPessoais
-            nomeCliente={nomeCliente}
-            setNomeCliente={setNomeCliente}
-            sobrenomeCliente={sobrenomeCliente}
-            setSobrenomeCliente={setSobrenomeCliente}
-            emailCliente={emailCliente}
-            setEmailCliente={setEmailCliente}
-            telefoneCliente={telefoneCliente}
-            setTelefoneCliente={setTelefoneCliente}
-          />
+          <div className={styles.cardEtapa}>
+            <InfosPessoais
+              nomeCliente={nomeCliente}
+              setNomeCliente={setNomeCliente}
+              sobrenomeCliente={sobrenomeCliente}
+              setSobrenomeCliente={setSobrenomeCliente}
+              emailCliente={emailCliente}
+              setEmailCliente={setEmailCliente}
+              telefoneCliente={telefoneCliente}
+              setTelefoneCliente={setTelefoneCliente}
+            />
+            <div className={styles.botoesNavegacao}>
+              <button onClick={voltarPasso} disabled={passoAtual === 0}>
+                Voltar
+              </button>
+              <button onClick={proximoPasso}>
+                Próximo
+              </button>
+            </div>
+          </div>
         )}
-        {passoAtual === 1 && (
-          <EndereçoPedido
-            cep={cep}
-            setCep={setCep}
-            rua={rua}
-            setRua={setRua}
-            numResidencia={numResidencia}
-            setNumResidencia={setNumResidencia}
-            cidade={cidade}
-            setCidade={setCidade}
-            bairro= {bairro}
-            setBairro={setBairro}
-            complemento={complemento}
-            setComplemento={setComplemento}
-            estado={estado}
-            setEstado={setEstado}
-          />
-        )}
-        {passoAtual === 2 && (
-          <ConcluirPedido
-            nomeCliente={nomeCliente}
-            sobrenomeCliente={sobrenomeCliente}
-            emailCliente={emailCliente}
-            telefoneCliente={telefoneCliente}
-            cep={cep}
-            rua={rua}
-            numResidencia={numResidencia}
-            cidade={cidade}
-            estado={estado}
-            onConcluir={handleConcluir}
-          />
-        )}
-      </div>
 
-      <div className={styles.botoesNavegacao}>
-        <button onClick={voltarPasso} disabled={passoAtual === 0}>
-          Voltar
-        </button>
-        {passoAtual === nomePassos.length - 1 ? (
-          <button onClick={handleConcluir} disabled={carrinhoItens.length === 0}>
-            Finalizar Compra
-          </button>
-        ) : (
-          <button onClick={proximoPasso}>
-            Próximo
-          </button>
+        {passoAtual === 1 && (
+          <div className={styles.cardEtapa}>
+            <EndereçoPedido
+              cep={cep}
+              setCep={setCep}
+              rua={rua}
+              setRua={setRua}
+              numResidencia={numResidencia}
+              setNumResidencia={setNumResidencia}
+              cidade={cidade}
+              setCidade={setCidade}
+              bairro={bairro}
+              setBairro={setBairro}
+              complemento={complemento}
+              setComplemento={setComplemento}
+              estado={estado}
+              setEstado={setEstado}
+            />
+            <div className={styles.botoesNavegacao}>
+              <button onClick={voltarPasso}>
+                Voltar
+              </button>
+              <button onClick={proximoPasso}>
+                Próximo
+              </button>
+            </div>
+          </div>
         )}
+
+        {passoAtual === 2 && (
+          <div className={styles.cardEtapa}>
+            <div className={styles.resumoCarrinho}>
+              <h2>Revisão do Pedido</h2>
+              {carrinhoItens.length > 0 ? (
+                <ul>
+                  {carrinhoItens.map(item => (
+                    <li key={item.id}>
+                      <span>{item.nome} (x{item.quantidade})</span>
+                      <span>R$ {(item.preco * item.quantidade).toFixed(2)}</span>
+                    </li>
+                  ))}
+                </ul>
+              ) : (
+                <p>Seu carrinho está vazio.</p>
+              )}
+              <h3 >Total: R$ {totalPedido.toFixed(2)}</h3>
+            </div>
+            <ConcluirPedido
+              nomeCliente={nomeCliente}
+              sobrenomeCliente={sobrenomeCliente}
+              emailCliente={emailCliente}
+              telefoneCliente={telefoneCliente}
+              cep={cep}
+              rua={rua}
+              bairro={bairro}
+              numResidencia={numResidencia}
+              cidade={cidade}
+              estado={estado}
+              complemento={complemento}
+            />
+            <div className={styles.botoesNavegacao}>
+              <button onClick={voltarPasso}>
+                Voltar
+              </button>
+              <button onClick={handleConcluir} disabled={carrinhoItens.length === 0}>
+                Finalizar Compra
+              </button>
+            </div>
+          </div>
+        )}
+
+
+
       </div>
     </section>
   );
